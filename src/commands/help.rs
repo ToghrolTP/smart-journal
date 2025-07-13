7f77da41a8526e422164ffbf602fba08eb740104
toghroltp <toghroltheprogrammer@gmail.com>
2025-07-13 14:49:25 +0330

    Fix: change the description for -aj flag

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -4,5 +4,5 @@ pub fn exe_help() {
     println!("{}", "Usage: rusty_diary <command> [options]".green());
     println!("{}", "Commands:".blue());
     println!("  -h,  --help           Show this help message");
-    println!("  -aj, --add-journal    Show this help message");
+    println!("  -aj, --add-journal    Adds a journal");
 }
